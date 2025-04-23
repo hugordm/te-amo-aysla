1002b718b04aac8b4a15ccfda35cf33b72860771
hugordm <hugomelo742@gmail.com>
2025-04-22 22:41:26 -0300

    Primeiro commit - projeto de declaração para Aysla

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem(){
+    const mensagem = document.getElementById("mensagem");
+    mensagem.classList.remove("mensagem-escondida");
+    mensagem.classList.add("mensagem-visivel");
+}
